3ba7922bd247ea8c3bf7208671d5b5073bd20113
HemanthNandyala <satyanandyala98@gmail.com>
2024-11-21 06:30:55 +0530

    HelloWorld Controller

---
+++ src/main/java/com/scaler/Product/Service/Controller/sampleController.java
@@ -0,0 +1,27 @@
+package com.scaler.Product.Service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+//End point : GET /hello
+public class sampleController {
+    @GetMapping("/hello")
+    public String helloWorld(){
+      return "Hello world Nani";
+    }
+    // End point GET  /hello/{name}
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable("name")String name){
+        return "hello" + name;
+    }
+
+    //End Point GET show/{sowId}/seat/{seatID}
+    @GetMapping("/show/{showId}/seat/{seatId}")
+    public String seatID(@PathVariable("showId") String showId, @PathVariable("seatId") int seatId) {
+        return "Hemanth show ID is " + showId + "and the seat number is "+ seatId;
+
+    }
+}
